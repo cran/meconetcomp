26d3a00cef4c0b0a8ca66d75deba8f9ea5931a35
Chi Liu <liuchi0426@126.com>
2022-10-05 14:20:02 +0000

    version 0.2.0

--- R/import.R
+++ R/import.R
@@ -1,9 +1,11 @@
 #' @import microeco
+#' @importFrom R6 R6Class
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
 #' @importFrom dplyr full_join
 #' @importFrom igraph is_directed
 #' @importFrom reshape2 melt
+#' @importFrom ggpubr ggboxplot
 NULL
 
 # define globalVariables
